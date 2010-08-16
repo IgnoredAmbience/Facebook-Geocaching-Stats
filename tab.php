d9b6c9e89c1bc37dde26e614cdad67175f7d04a6
Thomas Wood <grand.edgemaster@gmail.com>
2010-08-16 18:18:29 +0100

    Ugh, dont die

--- tab.php
+++ tab.php
@@ -3,7 +3,7 @@ $nologin = true;
 require_once 'include.php';
 
 if(!isset($_REQUEST['fb_sig_profile_id'])) {
-  error("No profile id given", true);
+  error("No profile id given");
 } else {
   $uid = (int) $_REQUEST['fb_sig_profile_id'];
 
