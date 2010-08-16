6d5c4951954eb6d28bfaed0f6970ec155bc0f028
Thomas Wood <grand.edgemaster@gmail.com>
2010-08-16 18:17:40 +0100

    Add errors

--- tab.php
+++ tab.php
@@ -3,21 +3,21 @@ $nologin = true;
 require_once 'include.php';
 
 if(!isset($_REQUEST['fb_sig_profile_id'])) {
-  die();
+  error("No profile id given", true);
 } else {
   $uid = (int) $_REQUEST['fb_sig_profile_id'];
-}
 
+  $query = "SELECT * FROM ids WHERE fbid = '$uid';";
+  $result = sqlite_query($db, $query);
+  if(!sqlite_num_rows($result)) {
+    error("Profile id not found in database");
 
-$query = "SELECT * FROM ids WHERE fbid = '$uid';";
-$result = sqlite_query($db, $query);
-if(!sqlite_num_rows($result)) {
-
-} else {
-  $arr = sqlite_fetch_array($result);
-  $img = imgurl($arr['gcguid'], $arr['bartext']);
-  $links = links($arr['gcguid'], $arr['gcuname']);
-  echo "<img src='$img' /><br />$links";
+  } else {
+    $arr = sqlite_fetch_array($result);
+    $img = imgurl($arr['gcguid'], $arr['bartext']);
+    $links = links($arr['gcguid'], $arr['gcuname']);
+    echo "<img src='$img' /><br />$links";
+  }
 }
 
 footer();
